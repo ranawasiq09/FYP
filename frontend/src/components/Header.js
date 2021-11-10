2d6813f572f9303d30e0cb7e3d8443998af0f07c
mustafa20009 <mustafa.mehmood1999@gmail.com>
2021-11-11 00:36:16 +0500

    header change

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,63 +18,85 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>ProShop</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Route render={({ history }) => <SearchBox history={history} />} />
-            <Nav className="ml-auto">
-              <LinkContainer to="/cart" style={{ fontSize: "13px" }}>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect
+        className="pl-5 pr-3"
+      >
+        <LinkContainer to="/">
+          <Navbar.Brand>ProShop</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Route render={({ history }) => <SearchBox history={history} />} />
+          <Nav className="ml-auto">
+            <LinkContainer to="/cart" style={{ fontSize: "13px" }}>
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+            </LinkContainer>
+            {userInfo ? (
+              <NavDropdown
+                title={userInfo.name.replace(/\"/g, "")}
+                id="username"
+                style={{ fontSize: "13px" }}
+              >
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                {userInfo.isAdmin ? (
+                  <>
+                    <LinkContainer to="/admin/userlist">
+                      <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/productlist">
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/orderlist">
+                      <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/categorylist">
+                      <NavDropdown.Item>Category</NavDropdown.Item>
+                    </LinkContainer>
+                  </>
+                ) : (
+                  ""
+                )}
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to="/login" style={{ fontSize: "13px" }}>
                 <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
+                  <i className="fas fa-user"></i> Sign In
                 </Nav.Link>
               </LinkContainer>
-              {userInfo ? (
-                <NavDropdown
-                  title={userInfo.name}
-                  id="username"
-                  style={{ fontSize: "13px" }}
-                >
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to="/login" style={{ fontSize: "13px" }}>
-                  <Nav.Link>
-                    <i className="fas fa-user"></i> Sign In
-                  </Nav.Link>
+            )}
+            {/* {userInfo && userInfo.isAdmin && (
+              <NavDropdown
+                title="Admin"
+                id="adminmenu"
+                style={{ fontSize: "13px" }}
+              >
+                <LinkContainer to="/admin/userlist">
+                  <NavDropdown.Item>Users</NavDropdown.Item>
                 </LinkContainer>
-              )}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown
-                  title="Admin"
-                  id="adminmenu"
-                  style={{ fontSize: "13px" }}
-                >
-                  <LinkContainer to="/admin/userlist">
-                    <NavDropdown.Item>Users</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/productlist">
-                    <NavDropdown.Item>Products</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/orderlist">
-                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/categorylist">
-                    <NavDropdown.Item>Category</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+                <LinkContainer to="/admin/productlist">
+                  <NavDropdown.Item>Products</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/admin/orderlist">
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/admin/categorylist">
+                  <NavDropdown.Item>Category</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            )} */}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </header>
   );
