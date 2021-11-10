387942c7117faa4b9325eb0e12a4c5886653e477
mustafa20009 <mustafa.mehmood1999@gmail.com>
2021-11-11 01:50:28 +0500

    name changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
         className="pl-5 pr-3"
       >
         <LinkContainer to="/">
-          <Navbar.Brand>ProShop</Navbar.Brand>
+          <Navbar.Brand>E-DUKAN</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
